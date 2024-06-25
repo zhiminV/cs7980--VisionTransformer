dcd6f7669f99cc6254eb971aa4482839bd903af8
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-06-24 20:39:36 -0700

    show 12 feature with the height and width 64*64 for the input image

--- ViT model/VisualizeData.py
+++ ViT model/VisualizeData.py
@@ -1,82 +1,111 @@
-
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import tensorflow as tf
 from preprocess_data import get_dataset
 
-#Load the datasets
+# Load the datasets
 BATCH_SIZE = 32
 
-train_dataset = get_dataset('/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/train/*_ongoing_*.tfrecord', data_size=64, sample_size=32, batch_size=BATCH_SIZE, num_in_channels=12, compression_type=None, clip_and_normalize=True, clip_and_rescale=False, random_crop=True, center_crop=False)
+train_dataset = get_dataset(
+    '/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/train/*_ongoing_*.tfrecord', 
+    data_size=64, 
+    sample_size=64, 
+    batch_size=BATCH_SIZE, 
+    num_in_channels=12, 
+    compression_type=None, 
+    clip_and_normalize=True, 
+    clip_and_rescale=False, 
+    random_crop=True, 
+    center_crop=False
+)
 
-validation_dataset = get_dataset('/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/val/*_ongoing_*.tfrecord', data_size=64, sample_size=32, batch_size=BATCH_SIZE, num_in_channels=12, compression_type=None, clip_and_normalize=True, clip_and_rescale=False, random_crop=True, center_crop=False)
+validation_dataset = get_dataset(
+    '/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/val/*_ongoing_*.tfrecord', 
+    data_size=64, 
+    sample_size=64, 
+    batch_size=BATCH_SIZE, 
+    num_in_channels=12, 
+    compression_type=None, 
+    clip_and_normalize=True, 
+    clip_and_rescale=False, 
+    random_crop=True, 
+    center_crop=False
+)
 
-test_dataset = get_dataset('/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/test/*_ongoing_*.tfrecord', data_size=64, sample_size=32, batch_size=BATCH_SIZE, num_in_channels=12, compression_type=None, clip_and_normalize=True, clip_and_rescale=False, random_crop=True, center_crop=False)
+test_dataset = get_dataset(
+    '/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/test/*_ongoing_*.tfrecord', 
+    data_size=64, 
+    sample_size=64, 
+    batch_size=BATCH_SIZE, 
+    num_in_channels=12, 
+    compression_type=None, 
+    clip_and_normalize=True, 
+    clip_and_rescale=False, 
+    random_crop=True, 
+    center_crop=False
+)
 
-# visualize data:
-# TITLES = [
-#   'Elevation',
-#   'Wind\ndirection',
-#   'Wind\nvelocity',
-#   'Min\ntemp',
-#   'Max\ntemp',
-#   'Humidity',
-#   'Precip',
-#   'Drought',
-#   'Vegetation',
-#   'Population\ndensity',
-#   'Energy\nrelease\ncomponent',
-#   'Previous\nfire\nmask',
-#   'Fire\nmask'
-# ]
+# Titles for the features
 TITLES = [
-    'elevation',
-    'th',
-    'sph',
-    'pr',
-    'NDVI',
-    'PrevFireMask',
+    'Elevation',
+    'WindDirection',
+    'WindVelocity',
+    'MinTemp',
+    'MaxTemp',
+    'Humidity',
+    'Precipitation',
+    'Drought',
+    'Vegetation',
+    'PopulationDensity',
+    'ERC',
+    'PreFireMask',
+    'FireMask'
 ]
 
-def plot_samples_from_dataset(dataset: tf.data.Dataset, n_rows: int):
+def plot_sample_from_dataset(dataset: tf.data.Dataset):
     """
-    Plot 'n_rows' rows of samples from dataset.
+    Plot one row of samples from the dataset showing 12 features and fire mask.
 
     Args:
-        dataset (Dataset): Dataset from which to plot samples.
-        n_rows (int): Number of rows to plot.
+        dataset (tf.data.Dataset): Dataset from which to plot samples.
     """
     global TITLES
 
-    # Get batch
+    # Get a batch
     inputs, labels = None, None
     for elem in dataset:
         inputs, labels = elem
         break
 
-    fig = plt.figure(figsize=(15,6.5))
+    # Select the first sample
+    sample_inputs = inputs[0]
+    sample_label = labels[0]
 
-    # Variables for controllong the color map for the fire masks
+    fig, axs = plt.subplots(1, 13, figsize=(25, 5))
+
+    # Variables for controlling the color map for the fire masks
     CMAP = colors.ListedColormap(['black', 'silver', 'orangered'])
     BOUNDS = [-1, -0.1, 0.001, 1]
     NORM = colors.BoundaryNorm(BOUNDS, CMAP.N)
-    # Number of data variables
-    n_features = 5
-    for i in range(n_rows):
-        for j in range(n_features + 1):
-            plt.subplot(n_rows, n_features + 1, i * (n_features + 1) + j + 1)
-            if i == 0:
-                plt.title(TITLES[j], fontsize=13)
-            if j < n_features - 1:
-                plt.imshow(inputs[i, :, :, j], cmap='viridis')
-            if j == n_features - 1:
-                plt.imshow(inputs[i, :, :, -1], cmap=CMAP, norm=NORM)
-            if j == n_features:
-                plt.imshow(labels[i, :, :, 0], cmap=CMAP, norm=NORM)
-            plt.axis('off')
+
+    for j in range(12):
+        ax = axs[j]
+        ax.imshow(sample_inputs[:, :, j], cmap='viridis')
+        ax.set_title(TITLES[j], fontsize=13)
+        ax.axis('off')
+        # Add height and width annotations
+        ax.text(0.5, -0.1, f'{sample_inputs.shape[0]}x{sample_inputs.shape[1]}', size=12, ha='center', transform=ax.transAxes)
+
+    # Plot the fire mask
+    ax = axs[12]
+    ax.imshow(sample_label[:, :, 0], cmap=CMAP, norm=NORM)
+    ax.set_title(TITLES[12], fontsize=13)
+    ax.axis('off')
+    # Add height and width annotations
+    ax.text(0.5, -0.1, f'{sample_label.shape[0]}x{sample_label.shape[1]}', size=12, ha='center', transform=ax.transAxes)
+
     plt.tight_layout()
     plt.show()
-    
 
 # Example usage
-plot_samples_from_dataset(train_dataset, 5)
+plot_sample_from_dataset(train_dataset)
